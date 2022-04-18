70495df75c979d9186b4c04e12bade60e6d37d33
梵茜 <yaomengjie.ymj@alibaba-inc.com>
2022-04-18 11:53:37 +0800

    Lob0练习题-done

--- pkg/plugin/demo/make_codec.sh
+++ pkg/plugin/demo/make_codec.sh
@@ -5,7 +5,7 @@ function make_so {
   # compile the plugin
   CGO_ENABLED=1 go build  -mod readonly --buildmode=plugin  -o codec.so ./codec.go
   echo "implement me!"
-  exit -1
+#  exit -1
 }
 
 make_so
