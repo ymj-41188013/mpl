283697b3dbca359b6c2d22982833e8970d03fc13
梵茜 <yaomengjie.ymj@alibaba-inc.com>
2022-05-05 14:43:54 +0800

    Lob0-fix

--- pkg/plugin/demo/make_codec.sh
+++ pkg/plugin/demo/make_codec.sh
@@ -3,7 +3,7 @@
 function make_so {
   # todo
   # compile the plugin
-  CGO_ENABLED=1 go build  -mod readonly --buildmode=plugin  -o codec.so ./codec.go
+  CGO_ENABLED=1 GO111MODULE=on go build  -mod readonly --buildmode=plugin  -o codec.so ./codec.go
   echo "implement me!"
 #  exit -1
 }
